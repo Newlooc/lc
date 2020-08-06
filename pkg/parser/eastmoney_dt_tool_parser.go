67197bbf01465f355de664281379b43c39962105
lechuan.niu <lechuan.niu@daocloud.io>
2020-08-06 14:23:59 +0800

    Fix uppercase issue

--- pkg/parser/eastmoney_dt_tool_parser.go
+++ pkg/parser/eastmoney_dt_tool_parser.go
@@ -3,7 +3,6 @@ package parser
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	log "github.com/sirupsen/logrus"
 	"strconv"
 	"strings"
@@ -15,184 +14,184 @@ const (
 )
 
 type response struct {
-	info string `json:"info"`
+	Info string `json:"info"`
 }
 
-type dtmock struct {
-	code              string
-	cnname            string
-	totalround        int64
-	baseinvestamount  float64
-	investtype        string
-	finalamount       float64
-	profitpercent     float64
-	mockcalculatedate time.time
-	dtrecords         []*dtrecord
+type DTMock struct {
+	Code              string
+	CNName            string
+	TotalRound        int64
+	BaseInvestAmount  float64
+	InvestType        string
+	FinalAmount       float64
+	ProfitPercent     float64
+	MockCalculateDate time.Time
+	dtRecords         []*dtRecord
 }
 
-type dtrecord struct {
-	date   time.time
-	price  float64
-	amount float64
-	total  float64
+type dtRecord struct {
+	Date   time.Time
+	Price  float64
+	Amount float64
+	Total  float64
 }
 
 var (
-	errorunexpectedrawdata = errors.new("unexpected summary raw data")
+	ERRORUnexpectedRawData = errors.New("unexpected summary raw data")
 )
 
-func newdtmock() *dtmock {
-	return &dtmock{}
+func NewDTMock() *DTMock {
+	return &DTMock{}
 }
 
-func (dt *dtmock) parse(raw string) error {
-	raw = strings.trim(raw, " \n")
-	uselessheadindex := strings.index(raw, "{")
+func (dt *DTMock) Parse(raw string) error {
+	raw = strings.Trim(raw, " \n")
+	uselessheadindex := strings.Index(raw, "{")
 	if uselessheadindex == -1 {
-		log.witherror(errorunexpectedrawdata).error("raw data format error. %s", raw)
-		return errorunexpectedrawdata
+		log.WithError(ERRORUnexpectedRawData).Error("raw data format error. %s", raw)
+		return ERRORUnexpectedRawData
 	}
 
 	raw = raw[uselessheadindex:]
-	uselesstailindex := strings.lastindex(raw, ")")
+	uselesstailindex := strings.LastIndex(raw, ")")
 	if uselesstailindex == -1 {
-		log.witherror(errorunexpectedrawdata).error("raw data format error. %s", raw)
-		return errorunexpectedrawdata
+		log.WithError(ERRORUnexpectedRawData).Error("raw data format error. %s", raw)
+		return ERRORUnexpectedRawData
 	}
 	raw = raw[:uselesstailindex]
-	log.debug(raw)
+	log.Debug(raw)
 
 	response := &response{}
-	if err := json.unmarshal([]byte(raw), response); err != nil {
-		log.witherror(err).error("response should be jsonp body. %s.", raw)
+	if err := json.Unmarshal([]byte(raw), response); err != nil {
+		log.WithError(err).Error("response should be jsonp body. %s.", raw)
 		return err
 	}
 
-	dtmixitems := strings.split(response.info, " ")
+	dtmixitems := strings.Split(response.Info, " ")
 	rawsummarys := dtmixitems[0]
 	rawrecords := dtmixitems[1:]
 
-	if err := dt.parsesummary(rawsummarys); err != nil {
-		log.witherror(err).error("failed to parse summary info.")
+	if err := dt.parseSummary(rawsummarys); err != nil {
+		log.WithError(err).Error("failed to parse summary info.")
 		return err
 	}
 
-	if err := dt.parserecords(rawrecords); err != nil {
-		log.witherror(err).error("failed to parse dt record.")
+	if err := dt.parseRecords(rawrecords); err != nil {
+		log.WithError(err).Error("failed to parse dt record.")
 		return err
 	}
 
 	return nil
 }
 
-func (dt *dtmock) parserecords(raw []string) error {
+func (dt *DTMock) parseRecords(raw []string) error {
 	if len(raw) == 0 {
 		return nil
 	}
-	if dt.dtrecords == nil {
-		dt.dtrecords = make([]*dtrecord, 0, len(raw))
+	if dt.dtRecords == nil {
+		dt.dtRecords = make([]*dtRecord, 0, len(raw))
 	}
 
 	for _, recordstr := range raw {
-		parsedrecord := &dtrecord{}
-		recordpreparse := strings.split(strings.trimspace(recordstr), "_")
+		parsedrecord := &dtRecord{}
+		recordpreparse := strings.Split(strings.TrimSpace(recordstr), "_")
 		if len(recordpreparse) != 2 {
-			log.witherror(errorunexpectedrawdata).errorf("record unrecognised. %s.", recordstr)
-			//return errorunexpectedrawdata
+			log.WithError(ERRORUnexpectedRawData).Errorf("record unrecognised. %s.", recordstr)
+			//return ERRORUnexpectedRawData
 		} else {
-			date, err := time.parse(strings.trimspace(dateformat), recordpreparse[1])
+			date, err := time.Parse(strings.TrimSpace(dateformat), recordpreparse[1])
 			if err != nil {
-				log.witherror(err).errorf("failed to parse date, raw data: %s", recordstr)
+				log.WithError(err).Errorf("failed to parse date, raw data: %s", recordstr)
 				//return err
 			}
-			parsedrecord.date = date
+			parsedrecord.Date = date
 		}
 
-		recordinfo := strings.split(strings.trimspace(recordpreparse[0]), "~")
+		recordinfo := strings.Split(strings.TrimSpace(recordpreparse[0]), "~")
 		if len(recordinfo) != 4 {
-			log.witherror(errorunexpectedrawdata).errorf("record info unrecognised. %s.", recordstr)
-			//return errorunexpectedrawdata
+			log.WithError(ERRORUnexpectedRawData).Errorf("record info unrecognised. %s.", recordstr)
+			//return ERRORUnexpectedRawData
 		} else {
-			price, err := strconv.parsefloat(numstrnor(recordinfo[1]), 64)
+			price, err := strconv.ParseFloat(numstrnor(recordinfo[1]), 64)
 			if err != nil {
-				log.witherror(err).error("failed to parse record info price. raw data: %s.", recordstr)
+				log.WithError(err).Error("failed to parse record info price. raw data: %s.", recordstr)
 			}
-			parsedrecord.price = price
+			parsedrecord.Price = price
 
-			amount, err := strconv.parsefloat(numstrnor(recordinfo[2]), 64)
+			amount, err := strconv.ParseFloat(numstrnor(recordinfo[2]), 64)
 			if err != nil {
-				log.witherror(err).error("failed to parse record info amount. raw data: %s.", recordstr)
+				log.WithError(err).Error("failed to parse record info amount. raw data: %s.", recordstr)
 			}
-			parsedrecord.amount = amount
+			parsedrecord.Amount = amount
 
-			total, err := strconv.parsefloat(numstrnor(recordinfo[3]), 64)
+			total, err := strconv.ParseFloat(numstrnor(recordinfo[3]), 64)
 			if err != nil {
-				log.witherror(err).error("failed to parse record info total. raw data: %s.", recordstr)
+				log.WithError(err).Error("failed to parse record info total. raw data: %s.", recordstr)
 			}
-			parsedrecord.total = total
+			parsedrecord.Total = total
 		}
 
-		dt.dtrecords = append(dt.dtrecords, parsedrecord)
-		log.debugf("record parse successfully. %+v", parsedrecord)
+		dt.dtRecords = append(dt.dtRecords, parsedrecord)
+		log.Debugf("record parse successfully. %+v", parsedrecord)
 	}
 
 	return nil
 }
 
-func (dt *dtmock) parsesummary(raw string) error {
+func (dt *DTMock) parseSummary(raw string) error {
 
-	summarypreparse := strings.split(raw, "|")
+	summarypreparse := strings.Split(raw, "|")
 	if len(summarypreparse) != 8 {
-		log.witherror(errorunexpectedrawdata).errorf("raw: %s.", raw)
-		return errorunexpectedrawdata
+		log.WithError(ERRORUnexpectedRawData).Errorf("raw: %s.", raw)
+		return ERRORUnexpectedRawData
 	}
 
-	code := strings.trimspace(summarypreparse[0])
-	cnname := strings.trimspace(summarypreparse[1])
-	investtype := strings.trimspace(summarypreparse[4])
+	code := strings.TrimSpace(summarypreparse[0])
+	cnname := strings.TrimSpace(summarypreparse[1])
+	investtype := strings.TrimSpace(summarypreparse[4])
 
-	totalround, err := strconv.atoi(numstrnor(summarypreparse[2][:len(summarypreparse[2])-3]))
+	totalround, err := strconv.Atoi(numstrnor(summarypreparse[2][:len(summarypreparse[2])-3]))
 	if err != nil {
-		log.witherror(err).errorf("failed to parse totalround, raw data: %s", raw)
+		log.WithError(err).Errorf("failed to parse totalround, raw data: %s", raw)
 		return err
 	}
 
-	baseinvestamount, err := strconv.parsefloat(numstrnor(summarypreparse[3]), 64)
+	baseinvestamount, err := strconv.ParseFloat(numstrnor(summarypreparse[3]), 64)
 	if err != nil {
-		log.witherror(err).errorf("failed to parse baseinvestamount, raw data: %s", raw)
+		log.WithError(err).Errorf("failed to parse baseinvestamount, raw data: %s", raw)
 		return err
 	}
 
-	finalamount, err := strconv.parsefloat(numstrnor(summarypreparse[5]), 64)
+	finalamount, err := strconv.ParseFloat(numstrnor(summarypreparse[5]), 64)
 	if err != nil {
-		log.witherror(err).errorf("failed to parse finalamount, raw data: %s", raw)
+		log.WithError(err).Errorf("failed to parse finalamount, raw data: %s", raw)
 		return err
 	}
 
-	profitpercent, err := strconv.parsefloat(numstrnor(summarypreparse[6][:len(summarypreparse[6])-1]), 64)
+	profitpercent, err := strconv.ParseFloat(numstrnor(summarypreparse[6][:len(summarypreparse[6])-1]), 64)
 	if err != nil {
-		log.witherror(err).errorf("failed to parse profitpercent, raw data: %s", raw)
+		log.WithError(err).Errorf("failed to parse profitpercent, raw data: %s", raw)
 		return err
 	}
 
-	mockcalculatedate, err := time.parse(dateformat, strings.trimspace(summarypreparse[7]))
+	mockcalculatedate, err := time.Parse(dateformat, strings.TrimSpace(summarypreparse[7]))
 	if err != nil {
-		log.witherror(err).errorf("failed to parse mockcalculatedate, raw data: %s", raw)
+		log.WithError(err).Errorf("failed to parse mockcalculatedate, raw data: %s", raw)
 		return err
 	}
 
-	dt.code = code
-	dt.cnname = cnname
-	dt.totalround = int64(totalround)
-	dt.baseinvestamount = baseinvestamount
-	dt.investtype = investtype
-	dt.finalamount = finalamount
-	dt.profitpercent = profitpercent
-	dt.mockcalculatedate = mockcalculatedate
+	dt.Code = code
+	dt.CNName = cnname
+	dt.TotalRound = int64(totalround)
+	dt.BaseInvestAmount = baseinvestamount
+	dt.InvestType = investtype
+	dt.FinalAmount = finalamount
+	dt.ProfitPercent = profitpercent
+	dt.MockCalculateDate = mockcalculatedate
 
 	return nil
 }
 
 func numstrnor(giv string) string {
-	return strings.replaceall(strings.trim(giv, " \n"), ",", "")
+	return strings.ReplaceAll(strings.Trim(giv, " \n"), ",", "")
 }
